b45c423d43521bc19f708499583c4c6419c24352
Michael Sun <mtsun05@gmail.com>
2026-01-26 16:24:31 -0600

    fix: buttonlink redesign

--- src/components/ButtonLink.tsx
+++ src/components/ButtonLink.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 
-import { FiArrowUpRight } from "react-icons/fi";
-
 interface ButtonLinkProps {
   children: React.ReactNode;
   path: string;
@@ -13,11 +11,10 @@ const ButtonLink = ({ children, path, marginRight }: ButtonLinkProps) => {
     <a
       className={`${
         marginRight && "mr-3"
-      } group relative items-center flex flex-row text-white text-xl bg-black/50 ring ring-white/20 hover:pr-10 cursor-pointer text-nowrap rounded-2xl w-fit px-4 py-3 transition-all duration-300 gap-1`}
+      } group relative items-center flex flex-row text-white text-xl bg-neutral-800 border-[0.25px] border-neutral-400/30 hover:bg-neutral-900 hover:border-neutral-400/50 cursor-pointer text-nowrap rounded-full w-fit px-4 py-3 transition-all duration-300 gap-1`}
       href={path}
     >
       <span className="font-light">{children}</span>
-      <FiArrowUpRight className="absolute opacity-0 right-4 transition-all duration-300 translate-y-3 -translate-x-3 group-hover:opacity-100 group-hover:translate-x-0 group-hover:translate-y-0" />
     </a>
   );
 };
